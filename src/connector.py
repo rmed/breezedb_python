6a7aab23a423e2db3570e280e19020b30a08c060
RMed <rafamedgar@gmail.com>
2013-07-11 00:29:39 +0200

    Added custom exception handling to the modules. Started implementation on table operations

--- src/connector.py
+++ src/connector.py
@@ -20,29 +20,39 @@ import xml.etree.ElementTree as XML
 import os, shutil
 import operations
 
+class ConnectorException(Exception):
+    """Class for exceptions in Connector module."""
+    def __init__(self, value):
+        print 'Connector exception: ', value
+
 class Connector():
+    """Connector class.
+
+    Its purpose is to store the path to the database and call the 
+    required functions.
+
+    Attributes:
+        database -- database path
+    """
 
     def __init__(self, path):
-        """
-        Connector object.
-
-        Its purpose is to store the path to the database and call the 
-        required functions.
+        """Initialization of the Connector.
 
         Arguments:
-        path -- Absolute path to the database directory
+            path -- Absolute path to the database directory
         """
+
         self.database = path
 
     def get_tablelist():
-        """
-        Get a list of tables.
+        """Get a list of tables.
 
         This function returns a list of tables contained in the database.
         The tables are fetched from the root.breeze file.
         """
+
         tablelist = []
-        
+
         # Parse the root.breeze file
         root_breeze = os.path.join(self.database, 'root.breeze')
         tree = xmltree.parse(root_file)
@@ -57,18 +67,18 @@ class Connector():
         return tablelist
         
 def create_breezedb(path, name):
-    """
-    Create a database structure in the specified path.
+    """Create a database structure in the specified path.
 
     Arguments:
-    path -- Absolute path in which to create the database directory
+        path -- Absolute path in which to create the database directory
     """
+    
     # Check for write access in the specified path
     can_write = os.access(path, os.W_OK)
 
     if !can_write:
-        # Return error
-        return -1
+        # Raise exception
+        raise ConnectorException('cannot write to path')
 
     # Create the directory and an empty root.breeze file
     try:
@@ -83,32 +93,34 @@ def create_breezedb(path, name):
         output.write(XML.tostring(breeze_tag)
         output.close()
 
-    except:
-        # Return error
-        return -1
+    except OSError:
+        # Raise exception
+        raise ConnectorException('could not create base directory')
 
-    return 0
+    except IOError:
+        # Raise exception
+        raise ConnectorException('error creating the root.breeze file')
 
 def remove_breezedb(path):
-    """
-    Remove the breeze directory structure of the specified path.
+    """Remove the breeze directory structure of the specified path.
 
     Arguments:
-    path -- Absolute path to the database directory
+        path -- Absolute path to the database directory
     """
+
     # Check for write access and root.breeze in the specified path
     can_write = os.access(path, os.W_OK)
     breeze_root = os.path.join(path, 'root.breeze')
     is_breezedb = os.path.isfile(breeze_root)
 
     if !can_write or !is_breezedb:
-        # Return error
-        return -1
+        # Raise exception
+        raise ConnectorException('cannot remove')
 
     # Remove the directory
     try:
         shutil.rmtree(path)
-    except:
-        # Return error
 
-    return 0
+    except:
+        # Raise exception
+        raise ConnectorException('could not remove the database')
