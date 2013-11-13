23664f5bfbf03f4a9d615d013c7b96d8255c03c9
RMed <rafamedgar@gmail.com>
2013-11-13 10:11:27 +0100

    Added __version__

--- breezedb/_version.py
+++ breezedb/_version.py
@@ -17,9 +17,4 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from breezedb.db import *
-from breezedb.table import *
-from breezedb.field import *
-from breezedb.element import *
-from breezedb.breeze_exceptions import *
-from breezedb._version import __version__
+__version__ = "dev"
